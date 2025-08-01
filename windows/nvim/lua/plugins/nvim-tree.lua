a76ccd35a8414053b9d881baaa9d0d6539e34bfd
Miles Collins <milescollins9@gmail.com>
2025-08-01 11:50:05 -0500

    added themes

--- windows/nvim/lua/plugins/nvim-tree.lua
+++ windows/nvim/lua/plugins/nvim-tree.lua
@@ -2,7 +2,8 @@ return {
   "nvim-tree/nvim-tree.lua",
   opts = {
     filters = {
-      dotfiles = false, -- set to false to show dotfiles like .env, .gitignore
+      dotfiles = false, -- Show dotfiles by default
+      git_ignored = false, -- Show git ignored files by default
     },
   },
 }
--- windows/nvim/lua/plugins/snacks.lua
+++ windows/nvim/lua/plugins/snacks.lua
@@ -4,6 +4,10 @@ return {
     opts = {
       scroll = { enabled = false },
       cursor = { enabled = false },
+      explorer = {
+        show_hidden = true, -- Try this if filters doesn't work
+        show_ignored = true, -- And this
+      },
     },
   },
 }
